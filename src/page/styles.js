fc70d9a55d8ff9dd5586e9aff3dbb4400245cbe8
˜Gabrielle <biela.a09@gmail.com>
2021-06-18 19:57:44 -0300

    Adicionando Api, filter

--- src/page/styles.js
+++ src/page/styles.js
@@ -5,3 +5,15 @@ export const Title = styled.h1`
   color: #7e459c;
   font-family: Arial, Helvetica, sans-serif;
 `;
+export const Input = styled.input`
+
+`;
+export const Div = styled.div`
+
+`;
+export const P = styled.p`
+
+`;
+export const Img = styled.img`
+
+`;
